33b1bdf10250febc904fe78326a91be5610fa438
viettungvuong <viettungvuong@gmail.com>
2024-03-26 22:49:36 +0700

    tính trung bình

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,5 +1,25 @@
 const Student = require("../models/student");
+const Exam = require("../models/exam");
 
-const updateToStudent = (studentId, examScore) => {};
+const calculateStudentAvg = (studentId) => {
+  Exam.find({})
+    .populate({
+      path: "student",
+      match: { studentId: studentId },
+    })
+    .exec((err, exams) => {
+      if (err) {
+        return null;
+      }
+      var sum = 0.0;
+      const n = exams.length();
+      exams.forEach((exam, index) => {
+        sum += exam.score;
+      });
+      return sum / n;
+    });
+};
 
-const calculateStudentAvg = (studentId) => {};
+module.exports = {
+  calculateStudentAvg,
+};
