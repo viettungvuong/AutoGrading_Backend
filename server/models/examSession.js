e65a597e5141897edc373b6214e3b7f094314f38
viettungvuong <viettungvuong@gmail.com>
2024-03-29 15:09:58 +0700

    Chỉnh model

--- server/models/examSession.js
+++ server/models/examSession.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const examSessionSchema = new mongoose.Schema({
   exams: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exam" }],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 const ExamSession = mongoose.model("ExamSession", examSessionSchema);
