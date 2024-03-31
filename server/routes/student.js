4fc199ac8e6b26c5d023fa9893440c007be75258
viettungvuong <viettungvuong@gmail.com>
2024-03-31 22:52:02 +0700

    Đổi route

--- server/routes/student.js
+++ server/routes/student.js
@@ -2,15 +2,37 @@ const express = require("express");
 const router = express.Router();
 
 const Student = require("../models/student");
+const ExamSession = require("../models/examSession");
 
-router.get("/", async (req, res) => {
-  try {
-    // lay moi entries trong bang Student
-    const students = await Student.find({});
-    res.json(students);
-  } catch (err) {
-    res.status(500).json({ error: "Internal server error" });
-  }
+// router.get("/", async (req, res) => {
+//   try {
+//     // lay moi entries trong bang Student
+//     const students = await Student.find({});
+//     res.json(students);
+//   } catch (err) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
+router.get("/:email", async (req, res) => {
+  ExamSession.find({})
+    .populate("exams")
+    .exec((err, sessions) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      const studentNames = new Set();
+      sessions.forEach((session) => {
+        // duyệt qua từng session
+        session.exams.forEach((exam) => {
+          // duyệt qua từng exam
+          studentNames.add(exam.student.name); // trong exam có mục student từ đó có name
+        });
+      });
+
+      return res.status(201).json({ students: [...studentNames] });
+    });
 });
 
 router.post("/", async (req, res) => {
@@ -23,7 +45,7 @@ router.post("/", async (req, res) => {
       studentId: studentId,
     });
     await student.save();
-    res.status(201).json(student);
+    res.status(201).json({ students: student });
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   }
