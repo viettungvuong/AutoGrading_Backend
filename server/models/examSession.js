77973559eb6e3fd7222fda0452bd888fe84fdc94
viettungvuong <viettungvuong@gmail.com>
2024-04-07 15:58:04 +0700

    theem name

--- server/models/examSession.js
+++ server/models/examSession.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 const examSessionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
   exams: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exam" }],
   user: {
     type: mongoose.Schema.Types.ObjectId,
