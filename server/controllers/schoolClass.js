24293dd4957ff9f2cdaa47a126d77be152c3f876
viettungvuong <viettungvuong@gmail.com>
2024-04-21 17:44:40 +0700

    push student thay vi user

--- server/controllers/schoolClass.js
+++ server/controllers/schoolClass.js
@@ -1,5 +1,6 @@
 const SchoolClass = require("../models/schoolClass");
 const Student = require("../models/student");
+const User = require("../models/user");
 
 const getAllClassesOfUser = async (userEmail) => {
   try {
@@ -49,23 +50,38 @@ const studentsOfClass = async (classId, userEmail) => {
 };
 
 const studentJoinClass = async (code, userId, res) => {
-  const user = await User.findOne({ email: userId });
-  if (!user) {
-    return res.status(400).json({ error: "User does not exist" });
-  }
-  if (user.isStudent == false) {
-    return res.status(400).json({ error: "Not a student" });
-  }
+  // const user = await User.findOne({ email: userId });
+  // if (!user) {
+  //   return res.status(400).json({ error: "User does not exist" });
+  // }
+  // if (user.isStudent == false) {
+  //   return res.status(400).json({ error: "Not a student" });
+  // }
+  const student = Student.findOne({})
+    .populate({
+      path: "user",
+      match: { email: userId },
+    })
+    .then((student) => {
+      if (student) {
+        console.log("Found student:", student);
+      } else {
+        console.log("No student found with the given name.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
   const schoolClass = await SchoolClass.findOne({ code: code });
   if (!schoolClass) {
     return res.status(400).json({ error: "Class does not exist" });
   }
-  if (schoolClass.students.includes(user._id)) {
+  if (schoolClass.students.includes(student._id)) {
     return res
       .status(400)
       .json({ error: "User is already a member of this class" });
   }
-  schoolClass.students.push(user._id);
+  schoolClass.students.push(student._id);
   await schoolClass.save();
   return res.status(200).json({ message: "Joining class successfully" });
 };
